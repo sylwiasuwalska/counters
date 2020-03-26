c2c153f60db0a9da81fb63b30d0ab0756747e61b
Sylwia Suwalska <sylwia.suwalska@gmail.com>
2020-03-26 19:07:38 +0100

    syntax formatted

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 var Counter = React.createClass({
     getInitialState: function() {
         return {
@@ -14,68 +12,86 @@ var Counter = React.createClass({
     },
 
     decrement: function() {
-    	this.setState({
-    		counter: this.state.counter -1
-    	});
+        this.setState({
+            counter: this.state.counter - 1
+        });
     },
 
     getDefaultProps: function() {
-    	console.log('Przekazanie domyślnych wartości do komponentu')
+        console.log("Przekazanie domyślnych wartości do komponentu");
     },
 
     componentWillMount: function() {
-    	console.log('Sprawdzenie, czy wszystkie propsy są przekazane poprawnie');
-  	},
+        console.log(
+            "Sprawdzenie, czy wszystkie propsy są przekazane poprawnie"
+        );
+    },
 
-  	sholudComponentUpdate: function() {
-    	console.log('Sprawdza, czy komponent powinien zostać jeszcze raz wyrenderowany.')
+    sholudComponentUpdate: function() {
+        console.log(
+            "Sprawdza, czy komponent powinien zostać jeszcze raz wyrenderowany."
+        );
     },
 
     componentWillUnmount: function() {
-    	console.log('Usuniecie komponentu ze strony, np. dodanie animacji')
+        console.log("Usuniecie komponentu ze strony, np. dodanie animacji");
     },
 
-
     render: function() {
-        return React.createElement('div', {},
-            React.createElement('h3', {}, 'Counter ' + this.state.counter),
-            React.createElement('button', {onClick: this.increment, className:'btn btn-dark button'}, "+"),
-			React.createElement('button', {onClick: this.decrement, className:'btn btn-dark button'}, "-"),
+        return React.createElement(
+            "div",
+            {},
+            React.createElement("h3", {}, "Counter " + this.state.counter),
+            React.createElement(
+                "button",
+                { onClick: this.increment, className: "btn btn-dark button" },
+                "+"
+            ),
+            React.createElement(
+                "button",
+                { onClick: this.decrement, className: "btn btn-dark button" },
+                "-"
+            )
         );
     }
-
-    
-
-
-    
 });
 
 var countersList = React.createClass({
-	render: function() {
-	return (
-	  React.createElement('div', {className: 'app'},
-	  	React.createElement('div', {className: 'row line'},
-		    React.createElement('div', {className: 'topLeft container col'}, 
-		    	React.createElement(Counter,{})
-		    ),
-		    React.createElement('div', {className: 'topRight container col'}, 
-		    	React.createElement(Counter,{})
-		    ),
-		),
-		React.createElement('div', {className: 'row line'},
-		    React.createElement('div', {className: 'botLeft container col'},
-		    	React.createElement(Counter,{})
-		    ),
-		    React.createElement('div', {className: 'botRight container col'}, 
-		    	React.createElement(Counter,{})
-		    ),
-		),
-	  )
-	)
-	}
+    render: function() {
+        return React.createElement(
+            "div",
+            { className: "app" },
+            React.createElement(
+                "div",
+                { className: "row line" },
+                React.createElement(
+                    "div",
+                    { className: "topLeft container col" },
+                    React.createElement(Counter, {})
+                ),
+                React.createElement(
+                    "div",
+                    { className: "topRight container col" },
+                    React.createElement(Counter, {})
+                )
+            ),
+            React.createElement(
+                "div",
+                { className: "row line" },
+                React.createElement(
+                    "div",
+                    { className: "botLeft container col" },
+                    React.createElement(Counter, {})
+                ),
+                React.createElement(
+                    "div",
+                    { className: "botRight container col" },
+                    React.createElement(Counter, {})
+                )
+            )
+        );
+    }
 });
 
-
-
 var app = React.createElement(countersList);
-ReactDOM.render(app, document.getElementById('app'));
+ReactDOM.render(app, document.getElementById("app"));
